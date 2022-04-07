a1f5aa7facf0092772961b1f58343e60fb891e1d
i1ntes <dbv594594@gmail.com>
2022-04-07 18:09:36 +0300

    Add MVP structure

---
+++ WeatherApp/Model/Weather.swift
@@ -0,0 +1,8 @@
+//
+//  Weather.swift
+//  WeatherApp
+//
+//  Created by Artim on 07.04.2022.
+//
+
+import Foundation
---
+++ WeatherApp/Presenter/Presenter.swift
@@ -0,0 +1,12 @@
+//
+//  Presenter.swift
+//  WeatherApp
+//
+//  Created by Artim on 07.04.2022.
+//
+
+import Foundation
+
+class Presenter {
+    
+}
