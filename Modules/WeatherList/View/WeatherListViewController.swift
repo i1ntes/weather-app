1e763c75ab992bc36c2a53df4d2977d827d0298c
i1ntes <dbv594594@gmail.com>
2022-04-14 20:57:24 +0300

    add tableView on first screen

--- Modules/WeatherList/View/WeatherListViewController.swift
+++ Modules/WeatherList/View/WeatherListViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class WeatherListViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
     
+    let city = ["Penza", "Moscow", "SPB"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setNavigationController()
+        tableView.register(UINib.init(nibName: "WeatherCell", bundle: nil), forCellReuseIdentifier: "WeatherCell")
+        tableView.dataSource = self
     }
     
     private func setNavigationController() {
@@ -26,4 +30,26 @@ class WeatherListViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
     }
 }
+
+extension WeatherListViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch tableView {
+        case self.tableView:
+            return self.city.count
+        default:
+            return 0
+        }
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath) as! WeatherCell
+        cell.cityLabel.text = self.city[indexPath.row]
+        return cell
+    }
+}
+
+
+
+
 
